9b76954c47b9f77c3da6ac0fa30535c42281be35
PiyushSingh12345 <piyushpgkp@gmail.com>
2025-05-19 18:32:03 +0530

    include usercontroller

--- routes/web.php
+++ routes/web.php
@@ -30,4 +30,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::resource('users', UserController::class);
+
 require __DIR__.'/auth.php';
